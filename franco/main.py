587d3748e32db87493f02aa0ed395d5a2f7484d8
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-11-22 20:17:40 +0200

    feat: add semantic clustering

--- franco/main.py
+++ franco/main.py
@@ -1,30 +1,26 @@
-from langchain_featherless_ai import ChatFeatherlessAi
-import os
-from dotenv import load_dotenv
+import pandas as pd
+import json
+from semantic_clustering import SemanticClusterer
 
-load_dotenv()
 
-api_key = os.environ.get("FEATHERLESS_API_KEY")
-api_url = "https://api.featherless.ai/v1"
-model = "Qwen/Qwen2.5-Coder-32B-Instruct"
+def load_data():
+    with open("data/data_english.json", "r") as f:
+        obj = json.load(f)
+    section = obj["Stairways"]
+    prompt = section["prompt"]
+    df = pd.DataFrame(section["data"])
+    return prompt, df
 
-llm = ChatFeatherlessAi(
-    api_key=api_key,
-    base_url=api_url,
-)
 
-system_prompt = "You are a helpful assistant that translates English to French. Translate the user sentence."
-user_prompt = "I love programming."
-messages = [
-    ("system", system_prompt),
-    ("human", user_prompt),
-]
+def main():
+    _, df = load_data()
+    clusterer = SemanticClusterer()
+    df["text"] = df["Title"] + ". " + df["Observation"]
+    df["category"] = clusterer.fit_predict(df["text"])
 
-response = llm.invoke(
-    messages,
-    model=model,
-    temperature=0.7,
-    seed=42,
-).content
+    print(df.head())
+    print(df["category"].value_counts())
 
-print(response)
+
+if __name__ == "__main__":
+    main()
